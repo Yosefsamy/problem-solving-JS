75cdbd9859584fd8bcafdee9d4d9603a4c2f2bc9
Yosefsamy <yoissefkaboria@gmail.com>
2024-09-15 23:20:42 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,54 @@
+// ! Ex1 : Filtering An Array ! //
+// function EvenNumbers(numbers){
+//     let Result = Numbers.filter(num => num % 2==0)
+//     return Result
+// }
+// let Numbers = [4,7,8,20,22,23,76,50,18]
+// console.log(EvenNumbers(Numbers));
+
+
+
+// ! Ex2 : Find The Largest Number ! //
+// function MaxNumber(numbers){
+//     let maxNumber = 0
+//     for(var i=0; i<numbers.length; i++){
+//         if(numbers[i] > maxNumber)
+//             maxNumber = numbers[i]
+//     }
+
+//     console.log(`Max Number : ${maxNumber} `);
+// }
+
+// let Array = [20,10,5,4,7,79,50,54,40]
+// MaxNumber(Array)
+
+
+
+// ! Ex3 : Reverse A String ! //
+// function ReverseString(word){
+//     let reversedString = ""
+//     for(var i=word.length-1; i>=0; i--){
+//         reversedString+=word[i]
+//     }
+//     return reversedString
+// }
+
+// let str = "Ahmed"
+// console.log(ReverseString(str))
+
+
+
+// ! Ex4 : Remove Duplicates ! //
+// function removeDuplicates(arr) {
+//     // Convert Set Into Array (The Set object automatically removes duplicate values)
+//     return [...new Set(arr)];
+//   }
+
+// let Numbers = [2,3,4,4,5,7,2,7,9,1,8,9,10]
+// console.log(removeDuplicates(Numbers))
+
+
+
+
+
+
